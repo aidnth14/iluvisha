75b3f8a9a581f05f153b68b4b42fccb45287fd09
Aiden Shoroz <aidenshoroz@gmail.com>
2026-09-25 01:58:19 +0600

    Add 3x3 grid pattern of Isha photo underneath balloon letters

--- src/main.ts
+++ src/main.ts
@@ -11,23 +11,87 @@ let currentTrackIndex = 0;
 
 if (app) {
   app.innerHTML = `
-    <!-- ISHA Realistic Physics Balloon Letters Only -->
-    <div class="isha-balloon-stage" id="balloon-stage" aria-label="ISHA">
-      <div class="balloon-item" data-index="0" data-letter="I">
-        <img src="/assets/balloon-i.png" alt="I" class="balloon-img" draggable="false" />
-        <div class="balloon-shadow"></div>
+    <div class="content-wrapper">
+      <!-- ISHA Realistic Physics Balloon Letters -->
+      <div class="isha-balloon-stage" id="balloon-stage" aria-label="ISHA">
+        <div class="balloon-item" data-index="0" data-letter="I">
+          <img src="/assets/balloon-i.png" alt="I" class="balloon-img" draggable="false" />
+          <div class="balloon-shadow"></div>
+        </div>
+        <div class="balloon-item" data-index="1" data-letter="S">
+          <img src="/assets/balloon-s.png" alt="S" class="balloon-img" draggable="false" />
+          <div class="balloon-shadow"></div>
+        </div>
+        <div class="balloon-item" data-index="2" data-letter="H">
+          <img src="/assets/balloon-h.png" alt="H" class="balloon-img" draggable="false" />
+          <div class="balloon-shadow"></div>
+        </div>
+        <div class="balloon-item" data-index="3" data-letter="A">
+          <img src="/assets/balloon-a.png" alt="A" class="balloon-img" draggable="false" />
+          <div class="balloon-shadow"></div>
+        </div>
       </div>
-      <div class="balloon-item" data-index="1" data-letter="S">
-        <img src="/assets/balloon-s.png" alt="S" class="balloon-img" draggable="false" />
-        <div class="balloon-shadow"></div>
-      </div>
-      <div class="balloon-item" data-index="2" data-letter="H">
-        <img src="/assets/balloon-h.png" alt="H" class="balloon-img" draggable="false" />
-        <div class="balloon-shadow"></div>
-      </div>
-      <div class="balloon-item" data-index="3" data-letter="A">
-        <img src="/assets/balloon-a.png" alt="A" class="balloon-img" draggable="false" />
-        <div class="balloon-shadow"></div>
+
+      <!-- 3x3 Grid Pattern of Isha (IMG_5496) Underneath the Letters -->
+      <div class="portrait-grid-container" id="portrait-grid">
+        <svg viewBox="0 0 540 680" class="portrait-grid-svg" aria-label="Isha portrait in 3x3 grid" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <clipPath id="cell-clip-0"><rect x="20" y="24" width="156" height="200" rx="26" ry="26" /></clipPath>
+            <clipPath id="cell-clip-1"><rect x="192" y="24" width="156" height="200" rx="26" ry="26" /></clipPath>
+            <clipPath id="cell-clip-2"><rect x="364" y="24" width="156" height="200" rx="26" ry="26" /></clipPath>
+            <clipPath id="cell-clip-3"><rect x="20" y="240" width="156" height="200" rx="26" ry="26" /></clipPath>
+            <clipPath id="cell-clip-4"><rect x="192" y="240" width="156" height="200" rx="26" ry="26" /></clipPath>
+            <clipPath id="cell-clip-5"><rect x="364" y="240" width="156" height="200" rx="26" ry="26" /></clipPath>
+            <clipPath id="cell-clip-6"><rect x="20" y="456" width="156" height="200" rx="26" ry="26" /></clipPath>
+            <clipPath id="cell-clip-7"><rect x="192" y="456" width="156" height="200" rx="26" ry="26" /></clipPath>
+            <clipPath id="cell-clip-8"><rect x="364" y="456" width="156" height="200" rx="26" ry="26" /></clipPath>
+          </defs>
+
+          <!-- 9 Interactive Cutout Cells -->
+          <g class="grid-cells-group">
+            <!-- Row 1 -->
+            <g class="grid-cell" data-cell="0">
+              <image href="/assets/IMG_5496.jpg" x="-95" y="-30" width="760" height="1030" preserveAspectRatio="none" clip-path="url(#cell-clip-0)" />
+              <rect x="20" y="24" width="156" height="200" rx="26" ry="26" class="grid-frame" />
+            </g>
+            <g class="grid-cell" data-cell="1">
+              <image href="/assets/IMG_5496.jpg" x="-95" y="-30" width="760" height="1030" preserveAspectRatio="none" clip-path="url(#cell-clip-1)" />
+              <rect x="192" y="24" width="156" height="200" rx="26" ry="26" class="grid-frame" />
+            </g>
+            <g class="grid-cell" data-cell="2">
+              <image href="/assets/IMG_5496.jpg" x="-95" y="-30" width="760" height="1030" preserveAspectRatio="none" clip-path="url(#cell-clip-2)" />
+              <rect x="364" y="24" width="156" height="200" rx="26" ry="26" class="grid-frame" />
+            </g>
+
+            <!-- Row 2 -->
+            <g class="grid-cell" data-cell="3">
+              <image href="/assets/IMG_5496.jpg" x="-95" y="-30" width="760" height="1030" preserveAspectRatio="none" clip-path="url(#cell-clip-3)" />
+              <rect x="20" y="240" width="156" height="200" rx="26" ry="26" class="grid-frame" />
+            </g>
+            <g class="grid-cell" data-cell="4">
+              <image href="/assets/IMG_5496.jpg" x="-95" y="-30" width="760" height="1030" preserveAspectRatio="none" clip-path="url(#cell-clip-4)" />
+              <rect x="192" y="240" width="156" height="200" rx="26" ry="26" class="grid-frame" />
+            </g>
+            <g class="grid-cell" data-cell="5">
+              <image href="/assets/IMG_5496.jpg" x="-95" y="-30" width="760" height="1030" preserveAspectRatio="none" clip-path="url(#cell-clip-5)" />
+              <rect x="364" y="240" width="156" height="200" rx="26" ry="26" class="grid-frame" />
+            </g>
+
+            <!-- Row 3 -->
+            <g class="grid-cell" data-cell="6">
+              <image href="/assets/IMG_5496.jpg" x="-95" y="-30" width="760" height="1030" preserveAspectRatio="none" clip-path="url(#cell-clip-6)" />
+              <rect x="20" y="456" width="156" height="200" rx="26" ry="26" class="grid-frame" />
+            </g>
+            <g class="grid-cell" data-cell="7">
+              <image href="/assets/IMG_5496.jpg" x="-95" y="-30" width="760" height="1030" preserveAspectRatio="none" clip-path="url(#cell-clip-7)" />
+              <rect x="192" y="456" width="156" height="200" rx="26" ry="26" class="grid-frame" />
+            </g>
+            <g class="grid-cell" data-cell="8">
+              <image href="/assets/IMG_5496.jpg" x="-95" y="-30" width="760" height="1030" preserveAspectRatio="none" clip-path="url(#cell-clip-8)" />
+              <rect x="364" y="456" width="156" height="200" rx="26" ry="26" class="grid-frame" />
+            </g>
+          </g>
+        </svg>
       </div>
     </div>
 
